0d869d81ced77ee654bb17abba28749c7fb77faa
Ben Futureworkz <ben.tran@futureworkz.com>
2016-11-07 12:58:22 +0700

    Add StorePicker components

---
+++ src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className='store-selector'>
+        <h2>Plase Enter A Store</h2>
+        <input type='text' required={true} placeholder='Store Name'/>
+        <button type='submit'>Visit Store</button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
