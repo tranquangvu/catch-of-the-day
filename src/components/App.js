80bcc67b7e849ef8ba72a5636283c904675be28b
Ben Futureworkz <ben.tran@futureworkz.com>
2016-11-07 13:37:09 +0700

    add simple header

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className='catch-of-the-day'>
         <div className='menu'>
-          <Header/>
+          <Header age="5000" cool={true} tagline='Fresh SeaFood Market'/>
         </div>
         <Order/>
         <Investory/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,17 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div>Header</div>
+      <header className='top'>
+        <h1>
+          Catch
+          <span className='ofThe'>
+            <span className='of'>of</span>
+            <span className='the'>the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className='tagline'>{this.props.tagline}</h3>
+      </header>
     );
   }
 }
